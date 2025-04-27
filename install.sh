e35613a396b444defab56ce0215c6277bb9c4f50
ModdGodd <98932563+ModdGodd@users.noreply.github.com>
2025-04-27 14:06:50 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ if [ "$(uname)" != "Darwin" ]; then
   exit 1
 fi
 
-mkdir -p "/tmp/Tritium"
-if [ ! -d "/Applications/Tritium.app" ]; then
-  echo "Tritium is not installed. Proceeding with installation."
+mkdir -p "/tmp/CleanUI"
+if [ ! -d "/Applications/CleanUI.app" ]; then
+  echo "CleanUI is not installed. Proceeding with installation."
 else
-  echo "Tritium is already installed. Deleting.."
-  rm -rf "/Applications/Tritium.app"
-  echo "Tritium has been deleted. Proceeding with installation."
+  echo "CleanUI is already installed. Deleting.."
+  rm -rf "/Applications/CleanUI.app"
+  echo "CleanUI has been deleted. Proceeding with installation."
 fi
-echo "Downloading Tritium..."
-curl -L -o "/tmp/Tritium/Tritium.zip" "https://github.com/Phantom8015/Tritium/releases/download/v2.0.0/Tritium-2.0.0-arm64-mac.zip"
-echo "Extracting Tritium..."
-unzip -o "/tmp/Tritium/Tritium.zip" -d "/tmp/Tritium"
-mv -f "/tmp/Tritium/Tritium.app" "/Applications"
-rm -rf "/tmp/Tritium"
+echo "Downloading CleanUI..."
+curl -L -o "/tmp/CleanUI/CleanUI.zip" "https://github.com/ModdGodd/CleanUI/releases/download/v2.0.0/CleanUI-2.0.0-arm64-mac.zip"
+echo "Extracting CleanUI..."
+unzip -o "/tmp/CleanUI/CleanUI.zip" -d "/tmp/CleanUI"
+mv -f "/tmp/CleanUI/CleanUI.app" "/Applications"
+rm -rf "/tmp/CleanUI"
 
-echo "Tritium has been successfully installed!"
+echo "CleanUI has been successfully installed!"
